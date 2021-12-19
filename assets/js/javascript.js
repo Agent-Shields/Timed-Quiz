da5cb468740fa42222f63d9594041241bca2bbf5
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2021-12-18 19:33:14 -0800

    initial counter functionality

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,21 @@
+//instantiate countdown timer
+var startCountdown = setInterval(countdown, 1000);
+var quizTimer = 75;
+var countdown = function() {
+    console.log(quizTimer);
+    quizTimer--;
+    if (quizTimer === 0) {
+        clearInterval(startCountdown);
+    }
+}
+
+
+// instantiate start button variable 
 var startBtn = document.querySelector("#start-btn");
 console.log(startBtn);
 
+
+// When start button clicked, start countdown function
 startBtn.addEventListener("click", function() {
-    window.alert("Start Button Clicked!")
-})
+    countdown();
+});
