6369f9938bc95f09b2deab64ddb48a2f8684fc5e
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2021-12-19 12:48:20 -0800

    timer decrement on click function works

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,21 +1,21 @@
-//instantiate countdown timer
-var startCountdown = setInterval(countdown, 1000);
-var quizTimer = 75;
-var countdown = function() {
-    console.log(quizTimer);
-    quizTimer--;
-    if (quizTimer === 0) {
-        clearInterval(startCountdown);
-    }
-}
-
-
 // instantiate start button variable 
 var startBtn = document.querySelector("#start-btn");
-console.log(startBtn);
-
+// console.log(startBtn);
 
 // When start button clicked, start countdown function
-startBtn.addEventListener("click", function() {
-    countdown();
-});
+startBtn.addEventListener("click", function(){
+    //instantiate countdown timer
+    var quizTimer = 5;
+
+    //decrement counter
+    var countdown = function(){
+        console.log(quizTimer);
+        quizTimer--;
+        if (quizTimer === 0){
+            clearInterval(startCountdown);
+        }
+    }
+    //decrement interval
+    var startCountdown = setInterval(countdown, 1000);
+})
+
